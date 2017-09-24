7d8c6116e64c0b69f923f387f4a26b76703abbea
Umair Siddique <umairsiddique@gmail.com>
2017-09-24 12:18:52 -0400

    Created initial version.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-export default 'Welcome to sendable'
+import request from 'request'
+
+export default class {
+  constructor(projectId, apiKey) {
+    this.projectId = projectId
+    this.apiKey = apiKey
+  }
+
+  render(templateId, params = {}) {
+    return request({
+      method: 'POST',
+      url: `https://api.sendable.io/v1/project/${this.projectId}/template/${templateId}/render`,
+      form: params,
+      headers: {
+        'Authorization': `ApiKey ${this.apiKey}`
+      },
+      json: true,
+    })
+  }
+
+  email(templateId, params = {}) {
+    return request({
+      method: 'POST',
+      url: `https://api.sendable.io/v1/project/${this.projectId}/template/${templateId}/email`,
+      form: params,
+      headers: {
+        'Authorization': `ApiKey ${this.apiKey}`
+      },
+      json: true,
+    })
+  }
+}
